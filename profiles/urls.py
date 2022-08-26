702ed85e15dc0b6fd01ee3031ba446947bffc361
anahita83radfar <anahita83radfar@gmail.com>
2022-08-26 18:35:17 +0000

    Add profiles detail view

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from profiles import views
 # The code taken from the Code Institute drf-api project
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
