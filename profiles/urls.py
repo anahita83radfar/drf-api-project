5e3983372ca1f3fb696137d10175f5abc1c4fe18
anahita83radfar <anahita83radfar@gmail.com>
2022-08-30 08:48:06 +0000

    Create PostList view, list all posts and create a post

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from profiles import views
 
-# The code taken from the Code Institute drf-api project
 urlpatterns = [
     path('profiles/', views.ProfileList.as_view()),
     path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
