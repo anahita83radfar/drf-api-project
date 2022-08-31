7111e79b958612e9f7c6f3f6a04b9e8797ba77f8
anahita83radfar <anahita83radfar@gmail.com>
2022-08-31 08:22:49 +0000

    Add create function to LikeSerializer to handle IntegrityError

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
 from .models import Like
+from django.db import IntegrityError
 
 
 # The code taken from the Code Institute drf-api project
 class LikeSerializer(serializers.ModelSerializer):
     """
     Serializer for the Like model.
-    We don't need a 'get_is_owner' method here because 
-    we don't need to know if the currently 
-    logged in user is the owner of a like.
+    We don't need a 'get_is_owner' method here because we don't 
+    need to know if the currently logged in user is the owner of a like.
+    The create method handles duplicates, the unique constraint on 'owner' and 'post'
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     
@@ -18,3 +19,11 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'post', 'created_at'
         ]
+        
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
