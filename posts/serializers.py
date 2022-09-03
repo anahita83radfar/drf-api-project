51335ba906d013451f128e8f46a00cadc4ec82b3
anahita83radfar <anahita83radfar@gmail.com>
2022-09-03 10:48:06 +0000

    Add the following_id and like_id fields to Profiles and Posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 # The code taken from the Code Institute drf-api project
@@ -8,11 +9,14 @@ class PostSerializer(serializers.ModelSerializer):
     Serializer for the Post model.
     'get_is_owner' method here check if the currently logged in user is the owner of the post.
     'validate_image' method will be validate the uploaded image every time we create or update a post.
+    By adding 'get_like_id' method we’ll know if the current user has already liked a post.
+    If they have, we’ll need a like_id field so that we know which one to delete if they wish to unlike it.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
     
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2 :
@@ -31,11 +35,21 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
     
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+    
+    
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'excerpt', 'content', 'image', 'is_owner', 'profile_id',
-            'profile_image', 'image_filter'
+            'profile_image', 'image_filter', 'like_id'
         ]
         
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 # The code taken from the Code Institute drf-api project
@@ -7,18 +8,33 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     Serializer for the Profile model.
     'get_is_owner' method here check if the currently logged in user is the owner of the profile.
+    By adding 'get_following_id' method If I am logged in and follow a profile, I can see the id 
+    of the newly created Follower instance in profile’s following_id field on the Profile list. 
+    This means that I am following that person’s profile and if I decide to unfollow  them, 
+    I know which Follower instance to delete.
     """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+    
+    
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'email', 'content', 'image', 'is_owner'
+            'email', 'content', 'image', 'is_owner', 'following_id'
         ]
         
